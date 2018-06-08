6b176611fabe5f399e8dd28c87e98d2364df3ee2
Mario Kleinsasser <mario.kleinsasser@gmail.com>
2018-06-08 12:05:15 +0200

    Move to GitLab.
    Signed-off-by: Mario Kleinsasser <mario.kleinsasser@gmail.com>

--- start-compose.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This startup script is needed, because otherwise
-# the ip address of the backend in the nginx.conf
-# will be wrong! For details, read Readme.md!
-
-sed -i -e "s/BACKENDIP/$1/g" nginx/conf/nginx.conf
-docker-compose up
-
--- tomcat/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<head><title>Basic Nginx Example</title></head>
-<body>
-Hello World @ <%=request.getRequestURL().toString()%>
-</body>
-</html>
